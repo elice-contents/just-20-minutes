5d9558654e0badd29c1458b48991d98f86e25687
Jungmin Yoo <jungminyoo@elicer.com>
2025-01-13 05:13:43 +0900

    Day 6: Carousel

---
+++ day06-carousel/main.js
@@ -0,0 +1,42 @@
+const carouselButtonPrev = document.getElementById("carousel-button-prev");
+const carouselButtonNext = document.getElementById("carousel-button-next");
+const carouselImages = document.getElementById("carousel-images");
+const slides = document.querySelectorAll(".carousel__image");
+
+let currentSlide = 1;
+
+carouselButtonPrev.addEventListener("click", () => {
+  if (currentSlide === 0) currentSlide = slides.length - 1;
+  const from = -(1024 * currentSlide);
+  const to = from + 1024;
+  carouselImages.animate(
+    {
+      left: [from + "px", to + "px"],
+    },
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  currentSlide--;
+});
+
+carouselButtonNext.addEventListener("click", () => {
+  if (currentSlide === slides.length - 1) currentSlide = 0;
+  const from = -(1024 * currentSlide);
+  const to = from - 1024;
+  carouselImages.animate(
+    {
+      left: [from + "px", to + "px"],
+    },
+    {
+      duration: 500,
+      easing: "ease",
+      iterations: 1,
+      fill: "both",
+    }
+  );
+  currentSlide++;
+});
